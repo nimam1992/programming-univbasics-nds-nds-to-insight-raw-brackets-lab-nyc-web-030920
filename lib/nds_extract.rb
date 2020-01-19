902b12668caee6e2aec4de1642cddd93ba3e4d88
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-01-19 01:31:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,6 +19,23 @@ def directors_totals(nds)
     rb_count+=1
   end
   rb_total
+
+  jc_total = 0
+  jc_count = 0
+  while jc_count < nds[2][:movies].length do
+    jc_total += nds[2][:movies][jc_count][:worldwide_gross]
+    jc_count+=1
+  end
+  jc_total
+
+  jc_total = 0
+  jb_count = 0
+  while jb_count < nds[2][:movies].length do
+    jb_total += nds[2][:movies][rb_count][:worldwide_gross]
+    jb_count+=1
+  end
+  jb_total
+
   result = {
     'Stephen Spielberg'=>ss_total,
     'Russo Brothers'=>rb_total
@@ -35,4 +52,3 @@ def directors_totals(nds)
   result
 end
 
-pp directors_database[0][:movies][0][:worldwide_gross]
