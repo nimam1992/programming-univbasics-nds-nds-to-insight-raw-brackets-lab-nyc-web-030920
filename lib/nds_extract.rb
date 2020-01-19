80beeb79913c04c1686d7eb5890e5acba8a3b771
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-01-19 01:16:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,11 +1,13 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
+directors_database
+
 def directors_totals(nds)
   ss_total = 0
   ss_count = 0
-  while ss_count < directors_database[0][:movies].length do
-    ss_total += directors_database[0][:movies][ss_count][:worldwide_gross]
+  while ss_count < nds[0][:movies].length do
+    ss_total += nds[0][:movies][ss_count][:worldwide_gross]
     ss_total
     ss_count+=1
   end
