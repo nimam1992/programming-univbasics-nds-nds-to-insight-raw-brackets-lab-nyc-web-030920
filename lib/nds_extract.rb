705573a1ebebbb04b3844d79d4f46d838be38b93
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-01-19 01:19:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,7 +8,6 @@ def directors_totals(nds)
   ss_count = 0
   while ss_count < directors_database[0][:movies].length do
     ss_total += directors_database[0][:movies][ss_count][:worldwide_gross]
-    ss_total
     ss_count+=1
   end
   ss_total
