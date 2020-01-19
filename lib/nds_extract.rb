65a4dc914d6f1b92ead4d81c343391efdc1b17bf
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-01-19 01:15:25 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,14 +4,14 @@ require 'directors_database'
 def directors_totals(nds)
   ss_total = 0
   ss_count = 0
-  while ss_count < nds[0][:movies].length do
-    ss_total += nds[0][:movies][ss_count][:worldwide_gross]
+  while ss_count < directors_database[0][:movies].length do
+    ss_total += directors_database[0][:movies][ss_count][:worldwide_gross]
     ss_total
     ss_count+=1
   end
   ss_total
   result = {
-    'Stepheh Spielberg'=>nds[0][:movies][0][:worldwide_gross]
+    'Stepheh Spielberg'=>[0][:movies][0][:worldwide_gross]
   }
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
